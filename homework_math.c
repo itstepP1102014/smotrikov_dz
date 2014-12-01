ef2dd8ebb35f3cc37bfd1e37a6f557c3056c8fde
smotrikov <dmitriy.smotrikov@gmail.com>
2014-12-01 11:11:45 +0300

    Update homework_math.c

--- homework_math.c
+++ homework_math.c
@@ -17,19 +17,23 @@ printf("their beer-maddened husbands.\n");
 // two
 /*
     int first_number,second_number;
+        printf("Enter first number\n");
         scanf("%d",&first_number);
+        printf("Enter second number\n");
         scanf("%d",&second_number);
-        printf("%d \n",first_number+second_number);
-        printf("%d \n",first_number-second_number);
-        printf("%d \n",first_number*second_number);
-        printf("%d \n",first_number/second_number);
-        printf("%d \n",first_number%second_number);
-        */
+        printf("%d + %d = %d \n",first_number,second_number,first_number+second_number);
+        printf("%d - %d = %d \n",first_number,second_number,first_number-second_number);
+        printf("%d * %d = %d \n",first_number,second_number,first_number*second_number);
+        printf("%d / %d = %d \n",first_number,second_number,first_number/second_number);
+        printf("%d %% %d = %d \n",first_number,second_number,first_number%second_number);
+*/
         //three
 
   /*  int R,H;
     double V,S;
+    printf("Enter first radius\n");
     scanf("%d",&R);
+    printf("Enter first height\n");
     scanf("%d",&H);
     V = (3.14*R*R*H)/3;
     S = 3.14*R*(sqrt(R*R+H*H)+R);
@@ -40,6 +44,7 @@ printf("their beer-maddened husbands.\n");
     // 4
     /*
     int a,b,c,d;
+    printf("Enter four numbers from space\n");
     scanf("%d %d %d %d",&a,&b,&c,&d);
     printf("%d\n %d\n  %d\n   %d\n",a*2,b*2,c*2,d*2);
     */
