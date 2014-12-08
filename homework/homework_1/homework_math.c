062958b2d09dcb7a489edc7a16d1ef6e7c24603f
smotrikov <dmitriy.smotrikov@gmail.com>
2014-12-08 14:59:55 +0300

    12
    2

---
+++ homework/homework_1/homework_math.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Math.h>
+int main()
+{
+/*
+// one
+printf("            And The Moon And The Stars And The World\n");
+printf("                        by Charles Bukowski\n");
+printf("Long walks at night--\n");
+printf("that’s what good for the soul:\n");
+printf("peeking into windows\n");
+printf("watching tired housewives\n");
+printf("trying to fight off\n");
+printf("their beer-maddened husbands.\n");
+    */
+// two
+/*
+    int first_number,second_number;
+        printf("Enter first number\n");
+        scanf("%d",&first_number);
+        printf("Enter second number\n");
+        scanf("%d",&second_number);
+        printf("%d + %d = %d \n",first_number,second_number,first_number+second_number);
+        printf("%d - %d = %d \n",first_number,second_number,first_number-second_number);
+        printf("%d * %d = %d \n",first_number,second_number,first_number*second_number);
+        printf("%d / %d = %d \n",first_number,second_number,first_number/second_number);
+        printf("%d %% %d = %d \n",first_number,second_number,first_number%second_number);
+*/
+        //three
+
+  /*  int R,H;
+    double V,S;
+    printf("Enter first radius\n");
+    scanf("%d",&R);
+    printf("Enter first height\n");
+    scanf("%d",&H);
+    V = (3.14*R*R*H)/3;
+    S = 3.14*R*(sqrt(R*R+H*H)+R);
+    printf("V : %.5f \n",V);
+    printf("S : %.5f \n",S);
+    */
+
+    // 4
+    /*
+    int a,b,c,d;
+    printf("Enter four numbers from space\n");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    printf("%d\n %d\n  %d\n   %d\n",a*2,b*2,c*2,d*2);
+    */
+    return 0;
+}
